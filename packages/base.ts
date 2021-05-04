aea68b00f1a5e23210cb629a9bbfbfe9f2861b69
yangss3 <yss_2016@outlook.com>
2021-05-05 00:06:09 +0800

    feat(packages): update the base composable api

--- packages/base.ts
+++ packages/base.ts
@@ -1,6 +1,6 @@
 import * as echarts from 'echarts'
 import { ECharts, EChartsOption } from 'echarts';
-import { h, onBeforeUnmount, onMounted, PropType, ref, shallowRef } from 'vue';
+import { h, nextTick, onBeforeUnmount, onMounted, PropType, ref, shallowRef } from 'vue';
 
 export const baseProps = {
   height: {
@@ -15,6 +15,10 @@ export const baseProps = {
     type: Boolean,
     default: true
   },
+  theme: {
+    type: String as PropType<'light' | 'dark'>,
+    default: 'light'
+  },
   // ECharts 标准配置对象
   option: {
     type: Object as PropType<EChartsOption>,
@@ -25,10 +29,32 @@ export const baseProps = {
 export function useChart(props: any) {
   const el = ref<HTMLDivElement>()
   const chart = shallowRef<ECharts>()
-  const resize = () => chart.value.resize()
-  onMounted(() => {
-    chart.value = echarts.init(el.value)
+  const contentWidth = ref(0)
+
+  const computeWidth = () => {
+    const width = chart.value?.getWidth() as number
+    const grid = chart.value?.getOption().grid as any
+    if (grid) {
+      const { left, right } = grid[0]
+      const leftWidth = typeof left === 'string'
+        ? width * Number.parseFloat(left) / 100
+        : left
+      const rightWidth = typeof right === 'string'
+        ? width * Number.parseFloat(right) / 100
+        : left
+      contentWidth.value = width - leftWidth - rightWidth
+    }
+  }
+
+  const resize = () => {
+    chart.value && chart.value.resize()
+    computeWidth()
+  }
+  onMounted(async () => {
+    chart.value = echarts.init(el.value!, props.theme)
     props.adaptive && window.addEventListener("resize", resize)
+    await nextTick()
+    computeWidth()
   })
   onBeforeUnmount(() => {
     props.adaptive && window.removeEventListener("resize", resize)
@@ -37,6 +63,7 @@ export function useChart(props: any) {
   return {
     el,
     chart,
+    contentWidth,
     render: () => h(
       'div',
       {
