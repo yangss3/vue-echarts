578c82c578c82ef774bd7859265669d2669038a2
yangss3 <yss_2016@outlook.com>
2021-05-05 00:18:53 +0800

    build: config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,23 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // build: {
-  //   lib: {
-  //     entry: path.resolve(__dirname, 'packages/index.ts'),
-  //     name: 'echarts-vue-components'
-  //   },
-  //   rollupOptions: {
-  //     // make sure to externalize deps that shouldn't be bundled
-  //     // into your library
-  //     external: ['vue', 'echarts'],
-  //     output: {
-  //       // Provide global variables to use in the UMD build
-  //       // for externalized deps
-  //       globals: {
-  //         vue: 'Vue',
-  //         echarts: 'echarts'
-  //       }
-  //     }
-  //   }
-  // }
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'packages/index.ts'),
+      name: 'vue-echarts'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', 'echarts'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          echarts: 'echarts'
+        }
+      }
+    }
+  }
 })
