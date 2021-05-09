4034b6f5c4be26ad95ac27b903c744dc4d6b4d99
yangss3 <yss_2016@outlook.com>
2021-05-09 18:37:50 +0800

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       // into your library
       external: ['vue', 'echarts'],
       output: {
+        dir: 'lib',
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
